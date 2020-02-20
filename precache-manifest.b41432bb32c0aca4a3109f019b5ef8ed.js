c225f47ab0c639e309b86035b117aa5c265d7594
vsvivekthangavelu <vivek.t@vsocialize.com>
2020-02-20 12:41:32 +0530

    Updates

---
+++ precache-manifest.b41432bb32c0aca4a3109f019b5ef8ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7071bf9c59dd91678f16a0d4630526c9",
+    "url": "/test-drive-app/index.html"
+  },
+  {
+    "revision": "dc312b285e8abc579fb5",
+    "url": "/test-drive-app/static/css/2.53c20c32.chunk.css"
+  },
+  {
+    "revision": "dc312b285e8abc579fb5",
+    "url": "/test-drive-app/static/js/2.3c1a1b43.chunk.js"
+  },
+  {
+    "revision": "3628f5b9f01ff204ba3fe2781466a3d6",
+    "url": "/test-drive-app/static/js/2.3c1a1b43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bef71e3d0c7cc6f140c7",
+    "url": "/test-drive-app/static/js/main.c6d8d37e.chunk.js"
+  },
+  {
+    "revision": "70920fd7e2bfe5a012b4",
+    "url": "/test-drive-app/static/js/runtime-main.b64c0a0c.js"
+  }
+]);
